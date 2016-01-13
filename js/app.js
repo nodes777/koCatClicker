982cc095cf52e1e3d5a5e079e67e287b02fbeb71
nodes777 <nodes777@gmail.com>
2016-01-13 16:50:00 -0500

    Adds with binding to clean up html

--- js/app.js
+++ js/app.js
@@ -31,7 +31,7 @@ var Cat = function() {
 var  ViewModel = function(){
 	this.currentCat = ko.observable(new Cat());
 	this.incrementCounter = function(){
-		this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+		this.clickCount(this.clickCount() + 1);
 	};
 }
 ko.applyBindings(new ViewModel());
